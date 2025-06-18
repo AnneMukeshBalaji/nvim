382877ddb2c8ccd3e2ee16e82ec2ff49d7c75ac5
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-18 03:56:51 +0000

    fixed erros in indent.lua file

--- lua/luffy/plugins/indent-blankline.lua
+++ lua/luffy/plugins/indent-blankline.lua
@@ -3,6 +3,14 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
-    indent = { char = "|" },
+    indent = { char = "│" },
   },
-} 
+  config = function(_, opts)
+    -- 🔧 Define the missing highlight group
+    vim.api.nvim_set_hl(0, "IblScope", { fg = "#5eacd3" }) -- tweak color to match theme
+
+    -- ✅ Now safe to call setup
+    require("ibl").setup(opts)
+  end,
+}
+
