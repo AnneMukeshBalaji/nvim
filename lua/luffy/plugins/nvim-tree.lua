c63510b7186e0984cd2da72eb1704091f82096cd
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-21 06:09:50 +0000

    changed ctrl-b to space-b

--- lua/luffy/plugins/nvim-tree.lua
+++ lua/luffy/plugins/nvim-tree.lua
@@ -42,7 +42,7 @@ return {
 
     -- Keymaps
     local keymap = vim.keymap
-    keymap.set("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>b", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
