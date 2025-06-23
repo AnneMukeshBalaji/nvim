148ce724bb5e92540019fc280cb8a92776ee041f
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-23 07:58:42 +0000

    removed continous comments

--- init.lua
+++ init.lua
@@ -4,3 +4,11 @@ vim.opt.shell = "/usr/bin/fish"  -- Or the correct path to your fish shell
 vim.o.timeout = true
 vim.o.timeoutlen = 200
 vim.opt.wrap = true
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
