1c6148e71e02e019a40b65ae46ca1dda185e8834
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-23 05:32:42 +0000

    added new schemes to colorschems.lua file

--- lua/luffy/plugins/colorscheme.lua
+++ lua/luffy/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
-  -- Catppuccin Theme
+
+  -- 🌈 Catppuccin Theme
   {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      flavour = "macchiato",
       transparent_background = true,
       styles = {
         comments = { "bold" },
@@ -37,42 +38,96 @@ return {
             StatusLine   = { bg = "NONE" },
             StatusLineNC = { bg = "NONE" },
             NormalFloat  = { bg = "NONE" },
-            Comment      = { fg = colors.flamingo ,bold = true },
+            Comment      = { fg = colors.flamingo, bold = true },
           }
         end,
       },
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin") -- Set Catppuccin as default
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 
-  -- Kanagawa Theme
+  -- 🐉 Kanagawa Theme
   {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
     lazy = true,
     priority = 900,
-    config = function()
-      require("kanagawa").setup({
-        transparent = true,
-        theme = "lotus", -- Options: "wave", "dragon", "lotus"
-        overrides = function(colors)
-          return {
-            Normal       = { bg = "NONE" },
-            NormalNC     = { bg = "NONE" },
-            EndOfBuffer  = { bg = "NONE" },
-            SignColumn   = { bg = "NONE" },
-            LineNr       = { bg = "NONE" },
-            CursorLineNr = { bg = "NONE" },
-            StatusLine   = { bg = "NONE" },
-            StatusLineNC = { bg = "NONE" },
-            NormalFloat  = { bg = "NONE" },
-          }
-        end,
-      })
+    opts = {
+      transparent = true,
+      theme = "wave", -- wave, dragon, lotus
+      overrides = function(colors)
+        return {
+          Normal       = { bg = "NONE" },
+          NormalNC     = { bg = "NONE" },
+          EndOfBuffer  = { bg = "NONE" },
+          SignColumn   = { bg = "NONE" },
+          LineNr       = { bg = "NONE" },
+          CursorLineNr = { bg = "NONE" },
+          StatusLine   = { bg = "NONE" },
+          StatusLineNC = { bg = "NONE" },
+          NormalFloat  = { bg = "NONE" },
+          Comment      = { fg = colors.palette.sumiInk4, bold = true },
+        }
+      end
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd.colorscheme("kanagawa")
+    end
+  },
+
+  -- 🧛 OneDark Theme
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    lazy = true,
+    priority = 900,
+    opts = {
+      style = "deep", -- dark, darker, cool, deep, warm, warmer, light
+      transparent = true,
+    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+      require("onedark").load()
     end,
   },
+
+  -- 🌃 TokyoNight Theme
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = true,
+    priority = 900,
+    opts = {
+      style = "night", -- storm, moon, night, day
+      transparent = true,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { bold = true },
+        variables = {},
+      },
+      on_highlights = function(hl, c)
+        hl.Normal       = { bg = "NONE" }
+        hl.NormalNC     = { bg = "NONE" }
+        hl.EndOfBuffer  = { bg = "NONE" }
+        hl.SignColumn   = { bg = "NONE" }
+        hl.LineNr       = { bg = "NONE" }
+        hl.CursorLineNr = { bg = "NONE" }
+        hl.StatusLine   = { bg = "NONE" }
+        hl.StatusLineNC = { bg = "NONE" }
+        hl.NormalFloat  = { bg = "NONE" }
+        hl.Comment      = { fg = c.comment, italic = true }
+      end
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight")
+    end
+  }
+
 }
 
