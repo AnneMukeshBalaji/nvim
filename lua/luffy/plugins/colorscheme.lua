680f3b0caad41d4cbe168c502009394fbcee9a74
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-25 12:14:13 +0000

    second final version you can publish for yourself

--- lua/luffy/plugins/colorscheme.lua
+++ lua/luffy/plugins/colorscheme.lua
@@ -1,33 +1,77 @@
+-- lua/plugins/colorscheme.lua
+
 return {
-  "folke/tokyonight.nvim",
-  name = "tokyonight",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night", -- Options: "storm", "moon", "night", "day"
-      transparent = true, -- Make background transparent
-      terminal_colors = true,
+  -- Tokyonight colorscheme
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night", -- Options: "storm", "moon", "night", "day"
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false, bold = true, fg = "#00cc00" },
+          keywords = { italic = false },
+          functions = { italic = false },
+          variables = { italic = false },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+
+      -- Uncomment this to enable Tokyonight
+      -- vim.cmd("colorscheme tokyonight")
+
+      -- Make autocomplete popup (cmp, coc, etc.) transparent
+      vim.cmd([[
+        highlight Pmenu guibg=NONE
+        highlight PmenuSel guibg=#283457 guifg=#ffffff
+        highlight PmenuSbar guibg=NONE
+        highlight PmenuThumb guibg=#3b4261
+        ]])
+    end,
+  },
+
+  -- Solarized Osaka colorscheme
+  {
+    "craftzdog/solarized-osaka.nvim",
+    name = "solarized-osaka",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
       styles = {
-        comments = { italic = false, bold = true, fg ='#00cc00' },
-        comments = { italic = false, bold = true, fg ='#5ed5e6' },
-        keywords = { italic = false},
-        functions = {italic = false},
-        variables = {italic = false},
-        sidebars = "transparent", -- also make sidebar (like NvimTree) transparent
-        floats = "transparent", -- make floating windows transparent
+        comments = { italic = false, bold = true, fg = "#00cc00" },
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+
+        sidebars = "transparent",
+        floats = "transparent",
       },
-    })
+      on_highlights = function(hl, c)
+        local none = "NONE"
+        hl.Normal       = { bg = none }
+        hl.NormalNC     = { bg = none }
+        hl.EndOfBuffer  = { bg = none }
+        hl.SignColumn   = { bg = none }
+        hl.LineNr       = { bg = none }
+        hl.CursorLineNr = { bg = none }
+        hl.WinSeparator = { bg = none }
+        hl.StatusLine   = { bg = none }
+        hl.StatusLineNC = { bg = none }
+        hl.NormalFloat  = { bg = none }
+      end,
+    },
+    config = function(_, opts)
+      require("solarized-osaka").setup(opts)
 
-    vim.cmd("colorscheme tokyonight")
-    -- Make autocomplete popup (cmp, coc, etc.) transparent
-    vim.cmd([[
-      highlight Pmenu guibg=NONE
-      highlight PmenuSel guibg=#283457 guifg=#ffffff
-      highlight PmenuSbar guibg=NONE
-      highlight PmenuThumb guibg=#3b4261
-      ]])
-  end,
+      -- Uncomment this to enable Solarized Osaka
+      vim.cmd.colorscheme("solarized-osaka")
+    end,
+  },
 }
-
 
