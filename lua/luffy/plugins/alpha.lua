7b4a57fbb44054e5dd89a03d139329b191b85376
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-25 16:01:47 +0000

    Reconnect to repo

---
+++ lua/luffy/plugins/alpha.lua
@@ -0,0 +1,45 @@
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- Set header with Batman ASCII
+    dashboard.section.header.val = {
+      "           _                         _",
+      "       _==/          i     i          \\==",
+      "     /XX/            |\\___/|            \\XX\\",
+      "   /XXXX\\            |XXXXX|            /XXXX\\",
+      "  |XXXXXX\\_         _XXXXXXX_         _/XXXXXX|",
+      " XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX",
+      "|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|",
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+      "|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|",
+      " XXXXXX/^^^^\"\\XXXXXXXXXXXXXXXXXXXXX/^^^^^\\XXXXXX",
+      "  |XXX|       \\XXX/^^\\XXXXX/^^\\XXX/       |XXX|",
+      "    \\XX\\       \\X/    \\XXX/    \\X/       /XX/",
+      "       \"\\       \"      \\X/      \"       /\"",
+      "                  Zach Bolinger",
+      "",
+      "    \"I'm whatever Gotham needs me to be.\" – Batman",
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  > Restore Session", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+    }
+
+    -- Apply config
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
+
---
+++ lua/luffy/plugins/colorscheme.lua
@@ -0,0 +1,93 @@
+-- lua/plugins/colorscheme.lua
+
+return {
+  -- Tokyonight colorscheme
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night", -- Options: "storm", "moon", "night", "day"
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false, bold = true, fg = "#00cc00" },
+          keywords = { italic = false },
+          functions = { italic = false },
+          variables = { italic = false },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+        on_highlights = function(hl, c)
+          -- Remove italics from default highlight groups
+          hl.Comment = { fg = "#00cc00", italic = false, bold = true }
+          hl.Function = { italic = false }
+          hl.Keyword = { italic = false }
+          hl.Type = { italic = false }
+          hl.Variable = { italic = false }
+          hl.Identifier = { italic = false }
+        end,
+      })
+
+      -- Uncomment this to enable Tokyonight
+      vim.cmd("colorscheme tokyonight")
+
+      -- Make autocomplete popup (cmp, coc, etc.) transparent
+      vim.cmd([[
+        highlight Pmenu guibg=NONE
+        highlight PmenuSel guibg=#283457 guifg=#ffffff
+        highlight PmenuSbar guibg=NONE
+        highlight PmenuThumb guibg=#3b4261
+      ]])
+    end,
+  },
+
+  -- Solarized Osaka colorscheme
+  {
+    "craftzdog/solarized-osaka.nvim",
+    name = "solarized-osaka",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        comments = { italic = false, bold = true, fg = "#00cc00" },
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, c)
+        local none = "NONE"
+        hl.Normal       = { bg = none }
+        hl.NormalNC     = { bg = none }
+        hl.EndOfBuffer  = { bg = none }
+        hl.SignColumn   = { bg = none }
+        hl.LineNr       = { bg = none }
+        hl.CursorLineNr = { bg = none }
+        hl.WinSeparator = { bg = none }
+        hl.StatusLine   = { bg = none }
+        hl.StatusLineNC = { bg = none }
+        hl.NormalFloat  = { bg = none }
+
+        -- Remove italics from standard groups
+        hl.Comment = { fg = "#00cc00", italic = false, bold = true }
+        hl.Function = { italic = false }
+        hl.Keyword = { italic = false }
+        hl.Type = { italic = false }
+        hl.Variable = { italic = false }
+        hl.Identifier = { italic = false }
+      end,
+    },
+    config = function(_, opts)
+      require("solarized-osaka").setup(opts)
+
+      -- Uncomment this to enable Solarized Osaka
+      -- vim.cmd.colorscheme("solarized-osaka")
+    end,
+  },
+}
+
