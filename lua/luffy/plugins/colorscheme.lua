ab3c342813923d23068db3dea6e03c41d7ff0314
AnneMukeshBalaji <mukimukku@gmail.com>
2025-06-27 14:50:37 +0000

    changed to catpuccin

--- lua/luffy/plugins/colorscheme.lua
+++ lua/luffy/plugins/colorscheme.lua
@@ -1,62 +1,67 @@
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    name = "solarized-osaka",
-    lazy = false,          
-    priority = 1000,       
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     opts = {
-      ------------------------------------------------------------------
-      -- 1. Core toggle
-      ------------------------------------------------------------------
-      transparent = true,      
-
-      ------------------------------------------------------------------
-      -- 2. Fine‑tune what should be see‑through 
-      ------------------------------------------------------------------
+      flavour = "mocha", -- or "latte", "frappe", "macchiato"
+      transparent_background = true,
+      no_italic = true,
       styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = {italic = false },
-        variables = {italic = false },
-        sidebars = "transparent",   
-        floats   = "transparent",   
+        comments = { "bold" },
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = { "bold" },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
       },
-
-      ------------------------------------------------------------------
-      -- 3. Turn off all italics
-      ------------------------------------------------------------------
-      disable_italics = true,
-
-      ------------------------------------------------------------------
-      -- 4. (Optional) wipe residual UI backgrounds + custom highlights
-      ------------------------------------------------------------------
-      on_highlights = function(hl, c)
-        local none = "NONE"
-        -- main editing area
-        hl.Normal       = { bg = none }
-        hl.NormalNC     = { bg = none }
-        hl.EndOfBuffer  = { bg = none }
-        -- line numbers & gutters
-        hl.SignColumn   = { bg = none }
-        hl.LineNr       = { bg = none }
-        hl.CursorLineNr = { bg = none }
-        -- splits / borders / statuslines
-        hl.WinSeparator = { bg = none }
-        hl.StatusLine   = { bg = none }
-        hl.StatusLineNC = { bg = none }
-        -- floating windows
-        hl.NormalFloat  = { bg = none }
-        -- comments: skyblue and bold, no italic
-        hl.Comment = { fg = "#87CEEB", bold = true, italic = false }  -- SkyBlue
-      end,
+      highlight_overrides = {
+        mocha = function(C)
+          return {
+            -- main editing area
+            Normal = { bg = "NONE" },
+            NormalNC = { bg = "NONE" },
+            EndOfBuffer = { bg = "NONE" },
+            -- line numbers & gutter
+            SignColumn = { bg = "NONE" },
+            LineNr = { bg = "NONE" },
+            CursorLineNr = { bg = "NONE" },
+            -- borders and status
+            WinSeparator = { bg = "NONE" },
+            StatusLine = { bg = "NONE" },
+            StatusLineNC = { bg = "NONE" },
+            -- floating windows
+            NormalFloat = { bg = "NONE" },
+            -- comment style
+            Comment = { fg = C.blue, bold = true, italic = false },
+            -- wipe italics from other common groups
+            Function = { italic = false },
+            Identifier = { italic = false },
+            Type = { italic = false },
+            Statement = { italic = false },
+            Keyword = { italic = false },
+            Constant = { italic = false },
+            String = { italic = false },
+            Character = { italic = false },
+            PreProc = { italic = false },
+            Number = { italic = false },
+            Boolean = { italic = false },
+            Repeat = { italic = false },
+            Conditional = { italic = false },
+          }
+        end,
+      },
     },
-
-    --------------------------------------------------------------------
-    -- 5. Setup + apply the colourscheme
-    --------------------------------------------------------------------
     config = function(_, opts)
-      require("solarized-osaka").setup(opts)
-      vim.cmd.colorscheme("solarized-osaka")
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
