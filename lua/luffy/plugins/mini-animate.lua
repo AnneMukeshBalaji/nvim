7776f3d7b45c8b99a7da0ef7d4e45b90788e4ea2
AnneMukeshBalaji <mukimukku@gmail.com>
2025-07-02 06:35:36 +0000

    fixed mini.animate plugin

--- lua/luffy/plugins/mini-animate.lua
+++ lua/luffy/plugins/mini-animate.lua
@@ -1,6 +1,6 @@
 return{
   "echasnovski/mini.animate",
-  version = true, -- always use latest
+  version = false, -- always use latest
   event = "VeryLazy",
   config = function()
     require('mini.animate').setup({
