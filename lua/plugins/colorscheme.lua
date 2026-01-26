3096b626124d8a8f853675cc7689e0abb09dc389
AnneMukeshBalaji <mukimukku@gmail.com>
2026-01-26 08:47:06 +0000

    conerted to kanagawa theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,97 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- Load immediately
-    priority = 1000, -- Load before other plugins
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+
     config = function()
-      -- Setup the theme
-      require("tokyonight").setup({
-        style = "night",
-        transparent = true, -- Enable global transparency
+      require("kanagawa").setup({
+        theme = "wave",
+
+        transparent = true,
+
         styles = {
-          comments = { italic = false },
-          keywords = { bold = true },
-          functions = {},
-          variables = {},
-          -- Set these to "transparent" to remove the dark backgrounds
-          sidebars = "transparent",
-          floats = "transparent",
+          comment = { italic = false },
+          keyword = { bold = true },
         },
+
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+                bg_float = "none",
+                bg_sidebar = "none",
+                bg_statusline = "none",
+              },
+            },
+          },
+        },
+
+        overrides = function(colors)
+          return {
+            ------------------------------------------------------------------
+            -- FULL TRANSPARENCY CORE
+            ------------------------------------------------------------------
+            Normal = { bg = "none" },
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            SignColumn = { bg = "none" },
+            EndOfBuffer = { bg = "none" },
+
+            LineNr = { bg = "none" },
+            CursorLineNr = { bg = "none" },
+
+            StatusLine = { bg = "none" },
+            StatusLineNC = { bg = "none" },
+            WinBar = { bg = "none" },
+            WinBarNC = { bg = "none" },
+
+            ------------------------------------------------------------------
+            -- VISUAL SELECTION MUST NOT BE TRANSPARENT
+            ------------------------------------------------------------------
+            Visual = { bg = colors.palette.waveBlue1 },
+            VisualNOS = { bg = colors.palette.waveBlue1 },
+
+            ------------------------------------------------------------------
+            -- CURSOR LINE (OPTIONAL BUT RECOMMENDED)
+            ------------------------------------------------------------------
+            CursorLine = { bg = colors.palette.sumiInk3 },
+
+            ------------------------------------------------------------------
+            -- POPUP MENU
+            ------------------------------------------------------------------
+            Pmenu = { bg = "none" },
+            PmenuSel = { bg = colors.palette.sumiInk4 },
+            PmenuBorder = { bg = "none" },
+
+            ------------------------------------------------------------------
+            -- TELESCOPE
+            ------------------------------------------------------------------
+            TelescopeNormal = { bg = "none" },
+            TelescopeBorder = { bg = "none" },
+            TelescopePromptNormal = { bg = "none" },
+
+            ------------------------------------------------------------------
+            -- FILE TREE
+            ------------------------------------------------------------------
+            NvimTreeNormal = { bg = "none" },
+            NeoTreeNormal = { bg = "none" },
+            NeoTreeNormalNC = { bg = "none" },
+
+            ------------------------------------------------------------------
+            -- WHICHKEY
+            ------------------------------------------------------------------
+            WhichKeyFloat = { bg = "none" },
+          }
+        end,
       })
 
-      -- Load the colorscheme
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme("kanagawa")
+
+      -- Hard-enforce transparency after colorscheme loads
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
 }
